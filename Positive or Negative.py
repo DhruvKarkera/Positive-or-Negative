3c348493fb5826af1827e2f2638fe892fe1234a4
DhruvKarkera <110173380+DhruvKarkera@users.noreply.github.com>
2022-08-01 17:54:20 +0530

    Add files via upload

---
+++ Positive or Negative.py
@@ -0,0 +1,8 @@
+integer = int(input("Assign a random integer"))
+
+if (integer < 0):
+    print("It is a negative integer")
+elif (integer == 0):
+    print("The integer is 0")
+else:
+    print("It is a positive integer")
